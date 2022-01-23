b772555d59e43ab0c6e77650c711777a8d9f7943
Lena <Lena@mail.ru>
2022-01-23 20:28:29 +0700

    add_tabs

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,42 +1,45 @@
-const links = document.querySelectorAll('.header-menu__item a')
+const scrollFunc = () => {
+    const links = document.querySelectorAll('.header-menu__item a')
 
 
-seamless.polyfill();
+    seamless.polyfill();
 
-links.forEach((element) => {
-    element.addEventListener('click', (event) => {
-        event.preventDefault()
+    links.forEach((element) => {
+        element.addEventListener('click', (event) => {
+            event.preventDefault()
         
-        const id = element.getAttribute('href').substring(1)
+            const id = element.getAttribute('href').substring(1)
+            const section = document.getElementById(id)
+        
+            if (section) {
+            section.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            })
+            } else {
+                seamless.elementScrollIntoView(document.querySelector("#characteristics"), {
+                    behavior: "smooth",
+                    block: "center",
+                    inline: "center",
+                });
+            }
+        })
+})
+
+    const FullSpecifications = document.querySelector('.card-details__link-characteristics')
+
+    FullSpecifications.addEventListener('click', (event) => {
+        event.preventDefault();
+
+        const id = FullSpecifications.getAttribute('href').substring(1)
         const section = document.getElementById(id)
-        
+
         if (section) {
             section.scrollIntoView({
                 behavior: 'smooth',
                 block: 'start'
             })
-        } else {
-            seamless.elementScrollIntoView(document.querySelector("#characteristics"), {
-                behavior: "smooth",
-                block: "center",
-                inline: "center",
-            });
         }
     })
-})
-
-const FullSpecifications = document.querySelector('.card-details__link-characteristics')
-
-FullSpecifications.addEventListener('click', (event) => {
-    event.preventDefault();
-
-    const id = FullSpecifications.getAttribute('href').substring(1)
-    const section = document.getElementById(id)
-
-    if (section) {
-        section.scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-        })
-    }
-})
+}
+scrollFunc()
