5da66693c08edb8c35903f6ce2136d02fb68919f
Lena <Lena@mail.ru>
2022-01-23 12:31:25 +0700

    add_smooth_scroll

---
+++ scripts/scroll.js
@@ -0,0 +1,42 @@
+const links = document.querySelectorAll('.header-menu__item a')
+
+
+seamless.polyfill();
+
+links.forEach((element) => {
+    element.addEventListener('click', (event) => {
+        event.preventDefault()
+        
+        const id = element.getAttribute('href').substring(1)
+        const section = document.getElementById(id)
+        
+        if (section) {
+            section.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            })
+        } else {
+            seamless.elementScrollIntoView(document.querySelector("#characteristics"), {
+                behavior: "smooth",
+                block: "center",
+                inline: "center",
+            });
+        }
+    })
+})
+
+const FullSpecifications = document.querySelector('.card-details__link-characteristics')
+
+FullSpecifications.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const id = FullSpecifications.getAttribute('href').substring(1)
+    const section = document.getElementById(id)
+
+    if (section) {
+        section.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
+    }
+})
